f585bc41a9a3189144f7ceb67794a26affc430bf
Teerut Srithongdee <teerutsr@gmail.com>
2023-11-25 13:38:53 +0700

    Update Footer component styling and add author information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,9 @@ interface Props { }
 
 const Footer: NextPage<Props> = () => {
     return (
-        <div className='flex justify-center text-white'>
-            KU Tech x KU SAB
+        <div className='flex text-white flex-col items-center justify-center'>
+            <div><a className='font-bold underline' href="https://www.facebook.com/ku.t3ch" target='_blank'>KU Tech</a> x <a className='font-bold underline' href="https://www.facebook.com/kusab.bk" target='_blank'>KU SAB</a></div>
+            <div>Made by <a className='font-bold underline' target='_blank' href="https://www.instagram.com/teerut_1t">Teerut</a></div>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,7 +140,7 @@ export default function Home() {
                                 </AnimatePresence>
                             </div>
                         </div>
-                        <div className="absolute left-0 right-0 top-[15rem] z-10 mx-auto flex max-w-lg flex-col items-center justify-center gap-10">
+                        <div className="absolute left-0 right-0 top-[14rem] z-10 mx-auto flex max-w-lg flex-col items-center justify-center gap-10">
                             <AddKratong />
                             <Link href="/blessing" className="button-sm w-fit">คำอธิษฐานทั้งหมด</Link>
                             <Footer />
