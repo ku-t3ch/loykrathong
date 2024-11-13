d5cd990d39c961c92348702cc2e4c8591f33029a
lucky345 <luc.ky_tunchanok@hotmail.com>
2024-11-13 15:41:26 +0700

    Change pocketbase url from http to https

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const config = {
   },
   env: {
     cdn: "https://s3.tech.nisit.ku.ac.th/assets/loykrathong/2566",
-    pocketbase: "http://pocketbase:8090",
+    pocketbase: "https://pocketbase.tech.nisit.ku.ac.th",
     turnstileSiteKey: "0x4AAAAAAANcMRCzztWiMUsR",
   },
 };
